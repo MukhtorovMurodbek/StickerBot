7a280f072fd8510831a0b37622a557f7a5b36038
MukhtorovMurodbek <mukhtorovmurodbek@gmail.com>
2026-09-04 02:31:33 +0900

    StickerBot 1.2.2

--- family_link.py
+++ family_link.py
@@ -75,7 +75,7 @@ FAMILY_SCHEMA = "family"
 # Bumped with the family's version (see CHANGELOG.md) -- reported in
 # heartbeats so /status can show which bots are running stale code after a
 # partial deploy.
-VERSION = os.environ.get("FAMILY_VERSION", "1.2.1")
+VERSION = os.environ.get("FAMILY_VERSION", "1.2.2")
 
 HEARTBEAT_SECONDS = int(os.environ.get("FAMILY_HEARTBEAT_SECONDS", "30"))
 
